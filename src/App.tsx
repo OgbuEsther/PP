9dc2424c5b2b09ff5d9a9e77823917cc218007d9
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-05-22 15:29:57 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Hero from './components/home/Hero';
+
+function App() {
+  return (
+    <div className="App">
+   <Hero />
+       
+    </div>
+  );
+}
+
+export default App;
